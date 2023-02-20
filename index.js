81b8a196ed0ebcbaa2f2a4e06f028f219736be53
JosePortillo <jose.portillo.samayoa@gmail.com>
2023-02-20 13:03:30 -0600

    feat: :sparkles: HTTP request with axios v2

--- index.js
+++ index.js
@@ -13,11 +13,29 @@ app.get('/photos',async(req,res)=>{
 
         res.json(data)
     } catch (error) {
-        console.log(error);
+        res.status(500).json({message: 'Internal server Error'})
     }
 })
 
+app.get('/albums',async(req,res)=>{
+    try {
+        let responsealbums = await axios.get('https://jsonplaceholder.typicode.com/albums')
+        let { data } = responsealbums
+        res.json(data)
+    } catch (error) {
+        res.status(500).json({message: 'Internal server Error'})
+    }
+})
 
+app.get('/users', async(req,res)=>{
+    try {
+        let responseusers = await axios.get('https://jsonplaceholder.typicode.com/users')
+        let { data } = responseusers
+        res.json(data)
+    } catch (error) {
+        res.status(500).json({message: 'Internal server Error'})
+    }
+})
 
 
 app.listen(port, ()=>{
