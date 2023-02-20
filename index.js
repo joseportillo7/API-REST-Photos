6ece4e3039a362061677ce7fe29ebb3d4f5be5f2
JosePortillo <jose.portillo.samayoa@gmail.com>
2023-02-20 11:58:35 -0600

    feat: :sparkles: HTTP request with axios

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const { port } = require('./src/config')
+const axios = require('axios')
 
 const app = express()
+app.use(express.json())
+
+
+app.get('/photos',async(req,res)=>{
+    try {
+        let responsephotos = await axios.get('https://jsonplaceholder.typicode.com/photos')
+        let { data } = responsephotos;
+
+        res.json(data)
+    } catch (error) {
+        console.log(error);
+    }
+})
+
 
 app.listen(port, ()=>{
     console.log(`Servidor escuchando en el puerto http://localhost:${port}`);
