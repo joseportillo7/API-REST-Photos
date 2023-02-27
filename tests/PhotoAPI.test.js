8971183e763c571f63066bf71c95bd02e36d5910
JosePortillo <jose.portillo.samayoa@gmail.com>
2023-02-27 13:40:47 -0600

    test: Add test to GET request

---
+++ tests/PhotoAPI.test.js
@@ -0,0 +1,28 @@
+const request = require('supertest')
+const app = require('..')
+
+
+describe('GET request from /api/photos',()=>{
+    test('should be response statusCode 200 - /api/photos/', async()=>{
+        let response = await request(app).get('/api/photos/').send()
+        expect(response.statusCode).toBe(200)
+    })
+    test('should be response statusCode 200 - /api/photos/1', async()=>{
+        let response = await (await request(app).get('/api/photos/1')).sed()
+        expect(response.statusCode).toBe(200)
+    })
+})
+
+describe('GET request from /api/albums',()=>{
+    test('should be response statusCode 200 - /api/albums/title', async()=>{
+        let response = await (await request(app).get('/api/albums/enim')).send()
+        expect(response.statusCode).toBe(200)
+    })
+})
+
+describe('GET request from /api/users/name',()=>{
+    test('should be responde statusCode 200 - /api/users/name', async()=>{
+        let response = await request(app).get('/api/users/Leanne').send()
+        expect(response.statusCode).toBe(200)
+    })
+})
